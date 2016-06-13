2c3546f6636b463e3263892575d978cd65c843da
Victor <Victor@VhaVHugo>
2016-06-13 20:51:17 -0300

    Troca de nome do pacote

---
+++ DesignPatters/src/strategy/Funcionario.java
@@ -0,0 +1,41 @@
+package strategy;
+/**
+ *
+ * @author Victor
+ */
+class Funcionario {
+    public static final int DESENVOLVEDOR = 1;
+    public static final int GERENTE = 2;
+    public static final int DBA = 3;
+    protected int cargo;
+    protected CalculaImposto estrategiaDeCalculo;
+    private final double salarioBase;
+    
+    public Funcionario(int cargo, double salarioBase){
+        this.salarioBase = salarioBase;
+        switch (cargo){
+            case DESENVOLVEDOR:
+                estrategiaDeCalculo = new CalculoImpostoQuinzeOuDez();
+                cargo = DESENVOLVEDOR;
+                break;
+            case DBA:
+                estrategiaDeCalculo = new CalculoImpostoQuinzeOuDez();
+                cargo = DBA;
+                break;
+            case GERENTE:
+                estrategiaDeCalculo = new CalculoImpostoVinteOuQuinze();
+                cargo = GERENTE;
+                break;
+            default:
+                throw new RuntimeException("Cargo não encontrado :/");
+                
+        }
+    }
+    public double calcularSalarioComImposto(){
+        return estrategiaDeCalculo.calculaSalarioComImposto(this);
+    }
+
+    int getSalarioBase() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
