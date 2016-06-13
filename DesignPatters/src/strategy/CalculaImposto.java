f12151def87537ae6a6dd76c64edb6dcd2d0a5b3
Victor <Victor@VhaVHugo>
2016-06-13 20:58:42 -0300

    Interface

---
+++ DesignPatters/src/strategy/CalculaImposto.java
@@ -0,0 +1,11 @@
+package strategy;
+/**
+ *
+ * @author Victor
+ */
+public interface CalculaImposto {
+    double calculaSalarioComImpsto(Funcionario umFuncionario);
+
+    public double calculaSalarioComImposto(Funcionario aThis);
+
+}
