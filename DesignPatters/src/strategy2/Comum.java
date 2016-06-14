98210ff6f44fe3f843230d474df30abfe44fc4fc
Victor <Victor@VhaVHugo>
2016-06-13 21:09:56 -0300

    class Comum implements Acesso

---
+++ DesignPatters/src/strategy2/Comum.java
@@ -0,0 +1,11 @@
+package strategy2;
+/**
+ *
+ * @author Victor
+ */
+public class Comum implements Acesso{
+    public void painel(){
+        System.out.println("Painel Comum");
+                
+    }
+}
